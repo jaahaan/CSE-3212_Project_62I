d869d976336da74ad57caadefa9dddbd13ebd580
Lisa <cse_1912020239@lus.ac.bd>
2025-11-12 09:59:52 +0600

    Drawer, FloatingActionButton, Text, Center

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,13 +9,53 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("HomePage"),
         backgroundColor: Colors.blueGrey,
-        leading: Icon(Icons.home),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.person)),
+        // leading: Icon(Icons.home),
+        // actions: [
+        //   IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+        //   IconButton(onPressed: () {}, icon: Icon(Icons.person)),
+        // ],
+      ),
+      endDrawer: NavigationDrawer(
+        children: [
+          DrawerHeader(
+            child: UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.blueGrey),
+              accountName: Text("Name"),
+              accountEmail: Text("Email"),
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.home),
+            title: Text("Homepage"),
+          ),
+          Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.person),
+            title: Text("Profile"),
+          ),
         ],
       ),
-      body: Text("Homepage"),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blueGrey,
+        foregroundColor: Colors.white,
+        shape: CircleBorder(),
+        child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text(
+          "Homepage",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.blueGrey,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
